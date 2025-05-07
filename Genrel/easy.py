c9503ccdb22b546c8f6e843c994c8742432e9c09
Muhammed Swalih M <zzray485@gmail.com>
2025-05-07 20:41:50 +0530

    finding len of arr with recursion

--- Genrel/easy.py
+++ Genrel/easy.py
@@ -8,3 +8,29 @@ for _ in arr:
     count += 1
 
 print(count)
+
+
+# with while loop
+
+arr = [1, 2, 10, 20, 30, 40, 50]
+count = 0
+
+try:
+    while True:
+        _ = arr[count]
+        count += 1
+except IndexError:
+    pass
+
+print(count)
+
+# with recursion
+def find_length(arr, index=0):
+    try:
+        _ = arr[index]
+        return 1 + find_length(arr, index + 1)
+    except IndexError:
+        return 0
+    
+arr = [1, 3, 5, 6, 5, 4, 3,9]
+print("lenght with recursion: ", find_length(arr))
